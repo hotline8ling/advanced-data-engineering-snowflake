6f5acee39ac9b98e16cf6af153bb5573ec8565e2
Roj Gorospe <gorosperojgian@gmail.com>
2026-01-22 16:48:28 +0800

    Direct fix on Main branch

--- module-1/hamburg_weather/pipeline/data/load_tasty_bytes.sql
+++ module-1/hamburg_weather/pipeline/data/load_tasty_bytes.sql
@@ -63,6 +63,7 @@ CREATE OR ALTER TABLE {{env}}_tasty_bytes.raw_pos.country
    country VARCHAR(16777216),
    iso_currency VARCHAR(3),
    iso_country VARCHAR(2),
+   city_id NUMBER(19,0),
    city VARCHAR(16777216),
    city_population VARCHAR(16777216)
 );
